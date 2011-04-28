9a9f039d9bc62aa4f639f5ecd6e98c8e0dece2f9
Rowan Lewis <me@rowanlewis.com>
2011-04-28 15:15:15 +1000

    Added a better example.

---
+++ tests/test.google-symphony.php
@@ -0,0 +1,17 @@
+<?php
+	
+	/**
+	* Google for Symphony CMS
+	*
+	* Goes to google.com and searches for "Symphony CMS".
+	*/
+	class SymphonyTestGoogleSymphony extends WebTestCase {
+		public function testExample() {
+			$this->get('http://google.com');
+			$this->setField('q', 'Symphony CMS');
+			$this->click('I\'m Feeling Lucky');
+			$this->assertTitle(new PatternExpectation('/Symphony\./'));
+		}
+	}
+	
+?>
